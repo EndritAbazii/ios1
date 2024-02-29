184e5cb6c091fca34216e199c31d0c4d5d5be388
EndritAbazii <122377055+EndritAbazii@users.noreply.github.com>
2024-02-29 09:08:49 +0100

    Update ViewExam.swift

--- iOS_Xcode_Swift_Project/Project/ViewExam.swift
+++ iOS_Xcode_Swift_Project/Project/ViewExam.swift
@@ -2,8 +2,8 @@
 //  ViewExam.swift
 //  Project
 //
-//  Created by NAM HYUNWOO on 21/10/20.
-//  Copyright © 2020 NAM HYUNWOO. All rights reserved.
+//  Created by Endrit Abazi on 24/02/24.
+//  Copyright © 2024 Endrit Abazi. All rights reserved.
 //
 
 import UIKit
